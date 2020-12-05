22dbfca1611ee8782ece5425fe8574c7f4e64c61
pvphero <impvhero@163.com>
2020-12-05 09:50:50 +0800

    appbar的效果

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
 
-/**
- * (=> 当行行数的简写
- */
-void main() {
-  runApp(
-      MyApp(items: new List<String>.generate(1000, (index) => "Item $index")));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-
-  MyApp({Key key, @required this.items}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '电影海报',
       home: Scaffold(
-        appBar: new AppBar(
-          title: new Text('ListView'),
+        appBar: AppBar(
+          title: Text("电影"),
         ),
-        body: new ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return new ListTile(
-              title: new Text('${items[index]}'),
-            );
-          },
-        ),
+        body: new Text('电影海报实例'),
       ),
     );
   }
